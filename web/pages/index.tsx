26e9b58e8763bec74cda2bd5fdb49cfaed0336b8
Yarot Kell <mechild02@gmail.com>
2023-08-18 12:43:01 -0400

    [MISC] removing old React project framework.

--- web/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Main } from '../components/layouts/Main';
-import { createRoot } from 'react-dom';
-
-export default function Home() {
-   return <Main />;
-}
-
-const root = createRoot(<Home />);
-root.render(document.getElementById("app"))
